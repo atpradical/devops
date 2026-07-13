075adad723bde4697f3025fd7ff2084c99c7098d
atpradical <ivan.dolgikh1199@gmail.com>
2026-07-14 00:55:05 +0300

    add deploy-k8s

--- back/src/health/health.controller.ts
+++ back/src/health/health.controller.ts
@@ -13,7 +13,7 @@ export class HealthController {
   getHealth(): { status: 'ok'; version: string } {
     return {
       status: 'ok',
-      version: process.env.APP_VERSION ?? 'unknown',
+      version: process.env.APP_VERSION ?? 'unknown 123',
     };
   }
 }
