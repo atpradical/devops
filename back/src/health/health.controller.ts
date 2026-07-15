c2ad0a03ad05676d735b7cf87e0faff9dc823f91
atpradical <ivan.dolgikh1199@gmail.com>
2026-07-15 17:01:25 +0300

    add deploy-k8s

--- back/src/health/health.controller.ts
+++ back/src/health/health.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
-import { ApiExcludeController } from '@nestjs/swagger';
+import {Controller, Get, HttpException, HttpStatus} from '@nestjs/common';
+import {ApiExcludeController} from '@nestjs/swagger';
+import {ReadinessService} from "../readiness.service";
 
 // Health-эндпоинт для hw4: smoke-проверка и AI-агент сверяют version
 // с APP_VERSION на сервере. Подкручен из env-переменной APP_VERSION,
@@ -9,11 +10,26 @@ import { ApiExcludeController } from '@nestjs/swagger';
 @ApiExcludeController()
 @Controller('health')
 export class HealthController {
-  @Get()
-  getHealth(): { status: 'ok'; version: string } {
-    return {
-      status: 'ok',
-      version: process.env.APP_VERSION ?? 'unknown 4555555555555',
-    };
-  }
+    constructor(private readonly readinessService: ReadinessService) {
+    }
+
+    @Get()
+    getHealth(): { status: 'ok'; version: string } {
+        return {
+            status: 'ok',
+            version: process.env.APP_VERSION ?? 'unknown 4555555555555',
+        };
+    }
+
+    @Get('ready')
+    getReadiness(): string {
+        if (this.readinessService.isReady()) {
+            throw new HttpException(
+                'Backend is not ready. Check logs for details.',
+                HttpStatus.SERVICE_UNAVAILABLE
+            )
+        } // 503
+
+        return 'ready';
+    }
 }
